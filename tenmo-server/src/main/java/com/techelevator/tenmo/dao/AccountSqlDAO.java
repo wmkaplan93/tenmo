91238fa9dcf765efffded44c8adcfacc6a2d551c
Emily Nordquist <veeryth@gmail.com>
2020-07-01 11:01:50 -0400

    auth user changes

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountSqlDAO.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountSqlDAO.java
@@ -7,7 +7,12 @@ import org.springframework.stereotype.Service;
 public class AccountSqlDAO implements AccountDAO{
 	
     private JdbcTemplate jdbcTemplate;
-
+//SELECT * FROM accounts
+    //then get the balance, if passing around account, queryForRowSet()  
+    //look in HW
+    //SQLRowSet result = 
+    
+    
 	@Override
 	public Double returnBalance(long userId) {
         return jdbcTemplate.queryForObject("SELECT balance FROM accounts WHERE user_id = ?", double.class, userId);
